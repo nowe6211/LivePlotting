57cf102f261418ada1537922f3638d948a4e3eb8
nowe6211 <91561970+nowe6211@users.noreply.github.com>
2022-05-18 17:53:05 -0500

    Hours and Minutes Select
    Adjusts range select to have hours, minutes and seconds as well

--- plot3RangeBox.py
+++ plot3RangeBox.py
@@ -6,14 +6,24 @@ from matplotlib.widgets import TextBox, Slider, Button
 # live animating with live data with ability to call a range of a recent data
 
 range = 10
-
+minutes = 0
+hours = 0
+seconds = 10
 fig1, axs1 = plt.subplots()
 plt.subplots_adjust(bottom=0.4)
 x = []
 y = []
 
-ax_box = plt.axes([0.1, 0.19, 0.8, 0.07])
-rangeBox = TextBox(ax_box, "range", initial = 10)
+ax_boxH = plt.axes([0.1, 0.19, 0.1, 0.07])
+rangeBoxH = TextBox(ax_boxH, "Hours", initial = 0)
+
+ax_boxM = plt.axes([0.35, 0.19, 0.1, 0.07])
+rangeBoxM = TextBox(ax_boxM, "Minutes", initial = 0)
+
+ax_boxS = plt.axes([0.6, 0.19, 0.1, 0.07])
+rangeBoxS = TextBox(ax_boxS, "Seconds", initial = 10)
+
+
 
 def animate(k):
     latest_time = np.genfromtxt("test_decay.dat", usecols = 0)[-1]
@@ -25,12 +35,24 @@ def animate(k):
     
     axs1.clear()
     
-    if rangeBox.text == "":
-        range = 1
+    
+    
+    if rangeBoxH.text == "":
+        hours = 0
     else:
-        range = float(rangeBox.text)
+        hours = float(rangeBoxH.text)
+    
+    if rangeBoxM.text == "":
+        minutes = 0
+    else:
+        minutes = float(rangeBoxM.text)
 
+    if rangeBoxS.text == "":
+        minutes = 0
+    else:
+        seconds = float(rangeBoxS.text)
 
+    range = seconds + 60*minutes + 3600*hours 
    
     i=0
     j=0
