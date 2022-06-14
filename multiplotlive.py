40a83ea0abd778f092cc84cf16bc3e9e7e01cd22
nowe6211 <91561970+nowe6211@users.noreply.github.com>
2022-06-13 19:44:25 -0500

    grid index function used to place plots
    Uses function which takes in the dimensions of the grid and index of the data stream to place plots accordingly

--- multiplotlive.py
+++ multiplotlive.py
@@ -4,6 +4,10 @@ import matplotlib.animation as animation
 
 fig, axs = plt.subplots(3,3)
 
+def gL(index, rows, columns): #function to index times and temps to grid
+    row = int(index/columns)
+    columns = index%columns
+    return [row,columns]  
 
 times = [[] for i in range(7)]
 temps = [[] for i in range(7)]
@@ -15,15 +19,10 @@ def animate(k):
         times[i].append(np.genfromtxt("test.dat", usecols = i)[-1])
         temps[i].append(np.genfromtxt("test.dat", usecols = (i+7))[-1])
 
-    k = 0
-    for i in range(3):
-        for j in range (3):
-            if k > 6:
-                break
-            else:
-                axs[i,j].clear()
-                axs[i,j].plot(times[k],temps[k])
-                k = k +1
+    for i in range(7):
+        axs[gL(i,3,3)[0],gL(i,3,3)[1]].clear()
+        axs[gL(i,3,3)[0],gL(i,3,3)[1]].plot(times[i],temps[i])
+
 
 
 ani = animation.FuncAnimation(fig, animate, interval = 1500)
