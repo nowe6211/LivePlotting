2048eeec06eb1c9b8be844bb44670d7221cf715f
nowe6211 <91561970+nowe6211@users.noreply.github.com>
2022-06-14 14:46:31 -0500

    Update multiplotlive.py

--- multiplotlive.py
+++ multiplotlive.py
@@ -7,7 +7,7 @@ fig, axs = plt.subplots(3,3)
 def gL(index, rows, columns): #function to index times and temps to grid
     row = int(index/columns)
     columns = index%columns
-    return [row,columns]  
+    return (row,columns)  
 
 times = [[] for i in range(7)]
 temps = [[] for i in range(7)]
@@ -20,8 +20,8 @@ def animate(k):
         temps[i].append(np.genfromtxt("test.dat", usecols = (i+7))[-1])
 
     for i in range(7):
-        axs[gL(i,3,3)[0],gL(i,3,3)[1]].clear()
-        axs[gL(i,3,3)[0],gL(i,3,3)[1]].plot(times[i],temps[i])
+        axs[gL(i,3,3)].clear()
+        axs[gL(i,3,3)].plot(times[i],temps[i])
 
 
 
